6c7ff987ae5bd6c30b9ecfbef50abf02107c24d8
wenchaogang <960726316@qq.com>
2023-03-12 18:17:11 +0800

    first xinxi

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello Worlddd!" );
+        System.out.println( "Hello wocququququ " );
         System.out.println( "Hello !" );
 
     }
